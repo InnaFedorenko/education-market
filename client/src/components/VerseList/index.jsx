f8ee155856aacdc03f31caf9f56c75cc2d8ab2e7
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-08-31 13:24:55 -0400

    implemented add order on UI

--- client/src/components/VerseList/index.jsx
+++ client/src/components/VerseList/index.jsx
@@ -4,18 +4,44 @@ import './style.css';
 import learnImagePath from "/images/verse/verse_example.png";
 import teachImagePath from "/images/verse/teach.png";
 import avatarPath from "/images/verse/Avatar_def.jpg";
+import { NavLink } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/client';
+import { ADD_ORDER } from '../../utils/mutations';
 
 const VerseList = ({ verses, title, type }) => {
   if (!verses.length) {
     return <h3>No Courses Yet</h3>;
   }
- verses = verses.filter(verse => verse.verseType.toString() == type);
- console.log(verses);
- const imagePath = type == "true" ? teachImagePath  : learnImagePath;
+  verses = verses.filter(verse => verse.verseType.toString() == type);
+  // console.log(verses);
+  // Mutation to add a new order
+  const [addOrder, {error}] = useMutation(ADD_ORDER);
+
+  const handlePlaceOrder = async (verseId) => {
+    try {
+      const { data } = await addOrder({
+        variables: {
+          verseTitle: verses.find((verse) => verse._id === verseId).title,
+          clientName: verses.find((verse) => verse._id === verseId).authorProfile.name,
+          clientEmail: verses.find((verse) => verse._id === verseId).authorProfile.email,
+          versePrice: verses.find((verse) => verse._id === verseId).price,
+        },
+      });
+
+   // refresh the page
+    // window.location.reload();
+
+
+    } catch (error) {
+      console.error('Error placing order:', error);
+    }
+  };
+
+  const imagePath = type == "true" ? teachImagePath : learnImagePath;
   return (
     <Container className='card-container nav_border'>
       <h3 className="text-left title">{title}</h3>
-      <Row  xs={1} sm={1} md={2} lg={3} >
+      <Row xs={1} sm={1} md={2} lg={3} >
         {verses.map((verse) => (
           <Col key={verse._id} className="mb-4">
             <Card className="card">
@@ -24,7 +50,7 @@ const VerseList = ({ verses, title, type }) => {
                 <Card.Title className='verse-title'>
                   <Row className="align-items-center">
                     <Col xs="auto">
-                      <img src={verse.authorProfile.avatarLink? verse.authorProfile.avatarLink : avatarPath} className="avatar" />
+                      <img src={verse.authorProfile.avatarLink ? verse.authorProfile.avatarLink : avatarPath} className="avatar" />
                     </Col>
                     <Col>
                       <p className="card-text-wrapper">{verse.authorProfile.name}</p>
@@ -35,26 +61,30 @@ const VerseList = ({ verses, title, type }) => {
 
                 <Card.Text className='verse-title'>{verse.title}</Card.Text>
                 <Card.Text>{verse.description}</Card.Text>
-                {/* <Card.Text className="card-text-wrapper" >Cost: ${verse.price}</Card.Text> */}
-                {/* <Card.Text>Verse Type = {verse.verseType.toString()}</Card.Text> */}
                 <Row>
                   <Col className="card-footer">
                     <Row className="align-items-center justify-content-between" >
                       <Col xs="auto">
-                      <Card.Text className="card-text-wrapper" >Cost: ${verse.price}</Card.Text>
-                        {/* <Button variant="primary" className="view-button">
-                          View
-                        </Button> */}
-                      </Col>
-                      <Col xs="auto" className="ordered-times-col">
-                        <p className="ordered-times">
+                        <Card.Text className="card-text-wrapper" >Cost: ${verse.price}</Card.Text>
+                        <Card.Text className="card-text-wrapper">
                           <span className="span">Ordered: </span>
                           <span className="text-wrapper-4">{verse.orderCount} </span>
                           <span className="text-wrapper-5">times</span>
-                        </p>
+                        </Card.Text>
+
+                      </Col>
+                      <Col xs="auto" className="ordered-times-col">
+
                       </Col>
                       <Col xs="auto">
-                        <Button variant="primary" className="order-button">
+                        <NavLink to={`/verse/${verse._id}`} className="view-button">
+                          View
+                        </NavLink>
+                        <Button
+                          variant="primary"
+                          className="order-button"
+                          onClick={() => handlePlaceOrder(verse._id)} // Call the handlePlaceOrder function on click
+                        >
                           Order
                         </Button>
                       </Col>
