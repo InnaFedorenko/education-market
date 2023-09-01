b03f7697a334bd68d2a188e5916f9f4005be6451
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-09-01 13:27:26 -0400

    refactoring of Learn and Teach pages

--- client/src/pages/Learn.jsx
+++ client/src/pages/Learn.jsx
@@ -3,24 +3,16 @@ import {QUERY_VERSES, QUERY_USER} from '../utils/queries';
 import VerseList from '../components/VerseList';
 
 const Learn = () => {
-  // const { loading: loading, data: data, refetch: refetch } = useQuery(QUERY_VERSES);
-  // const verses = data?.verses || [];
-  // const { loading: userLoading, data: userData , refetch: userRefetch} = useQuery(QUERY_USER);
-  // const profile = userData?.me || {};
-  //const clientName = profile.name;
+
     return (
       <main>
        <div >
-          {/* {loading ? (
-            <div>Loading...</div>
-          ) : ( */}
             <VerseList
               verses1= "" //{verses}
               profile1="" //{profile}
               title="Explore Learning Topics"
               type="false"
             />
-          {/* )} */}
       </div>
       </main>
     );
--- client/src/pages/Teach.jsx
+++ client/src/pages/Teach.jsx
@@ -4,23 +4,16 @@ import {QUERY_VERSES, QUERY_USER} from '../utils/queries';
 import VerseList from '../components/VerseList';
 
 const Teach = () => {
-  // const { loading, data } = useQuery(QUERY_VERSES);
-  // const verses = data?.verses || [];
-  // const { loading: userLoading, data: userData } = useQuery(QUERY_USER);
-  // const profile = userData?.me || {};
+
     return (
       <main>
        <div >
-          {/* {loading ? (
-            <div>Loading...</div>
-          ) : ( */}
             <VerseList
               verses1=""
               profile1=""
               title="Explore Teaching Topics"
               type="true"
-            />
-          {/* )} */}
+             />
       </div>
       </main>
     );
